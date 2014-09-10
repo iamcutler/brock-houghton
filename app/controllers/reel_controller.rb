76a9c640a70610ce6f31b86ba12913d29253b480
iamcutler <iamcutler@icloud.com>
2014-09-10 01:02:44 -0700

    Introduced reel controller

---
+++ app/controllers/reel_controller.rb
@@ -0,0 +1,4 @@
+class ReelController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  get 'reel', to: 'reel#index', as: :reel
 end
