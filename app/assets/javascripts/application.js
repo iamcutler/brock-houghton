2b6b145cd100b813c3244cf7763d3c45b20d77fc
iamcutler <iamcutler@icloud.com>
2014-09-10 15:19:42 -0700

    Moved videos js to app js and binding to page:load for turbo links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,14 +16,22 @@
 //= require html5shiv.min
 //= require_tree .
 
+$(document).on('page:load', function() {
+  $('section.videos ul li').on('mouseover mouseout', function(e) {
+    // Fade video play buttons
+    // Pass event to control fading of the button
+    VIDEOS.fadePlayBtn(e);
+  });
+});
+
 var VIDEOS = (function($) {
   var timing = 200;
 
   return {
     fadePlayBtn: function(event) {
-      var opacity = (event.type == 'mouseleave') ? 1.0 : 0.4;
+      var opacity = (event.type == 'mouseout') ? 1.0 : 0.4;
 
-      $("section.videos ul li .feature-container .play-button").animate({
+      $("section.videos ul li .feature-container .play-button").stop().animate({
         opacity: opacity
       }, timing)
     }
--- app/assets/javascripts/reel.js.coffee
+++ app/assets/javascripts/reel.js.coffee
@@ -1,11 +1,3 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-
-$(document).ready( ->
-  $('section.videos ul li').hover((e) ->
-    # Fade video play buttons
-    # Pass event to control fading of the button
-    VIDEOS.fadePlayBtn(e)
-  )
-)
