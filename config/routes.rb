331413624af165467b6c43e42a94d625674a4b57
iamcutler <iamcutler@icloud.com>
2014-09-10 08:30:26 -0700

    Introduced archive controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'reel', to: 'reel#index', as: :reel
+  get 'archival', to: 'archive#index', as: :archival
   get 'contact', to: 'contact#index', as: :contact
 end
