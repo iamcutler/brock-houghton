1351f76b166bd620d94923832a4a1633e5d3c590
iamcutler <iamcutler@icloud.com>
2014-09-11 08:50:00 -0700

    Added scopes to video model for reel and archival

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ActiveRecord::Base
   validates :title, :director, :order, presence: true
+
+  scope :reel, -> { where(archive: false) }
+  scope :archive, -> { where(archive: true) }
+  scope :order_asc, -> { order(:order) }
 end
