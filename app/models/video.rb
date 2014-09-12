ab1fd069b489839d888b1daa4221b9748c3a428f
iamcutler <iamcutler@icloud.com>
2014-09-12 16:11:03 -0700

    Added video model image attachment with thumb sizing

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,7 @@
 class Video < ActiveRecord::Base
   validates :title, :director, :order, presence: true
+  has_attached_file :image, :styles => { :thumb => "262X200>" }, :default_url => 'vid_missing.jpg'
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   scope :reel, -> { where(archive: false) }
   scope :archive, -> { where(archive: true) }
