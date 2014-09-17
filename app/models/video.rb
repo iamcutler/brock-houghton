457b872ed2935b5d6c399343f6d293680c79c756
iamcutler <iamcutler@icloud.com>
2014-09-17 08:40:30 -0700

    Added attach file image to video model.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,12 @@
 class Video < ActiveRecord::Base
   validates :title, :director, :order, presence: true
-  has_attached_file :image, :styles => { :thumb => "262X200>" }, :default_url => 'vid_missing.jpg'
+  has_attached_file :image,
+                    :styles => {
+                      :thumb => "262X200>"
+                    },
+                    :default_url => "vid_missing.jpg",
+                    :path => ":rails_root/public/system/images/uploads/:basename.:extension"
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   scope :reel, -> { where(archive: false) }
