56ea23d8924fa34b2bb1e049ac66d28705fff121
iamcutler <iamcutler@icloud.com>
2014-09-17 08:37:16 -0700

    Modified show form in admin panel for videos

--- app/admin/video.rb
+++ app/admin/video.rb
@@ -7,4 +7,18 @@ ActiveAdmin.register Video do
     column :created_at
     actions
   end
+
+  show do |video|
+    attributes_table do
+      row :title
+      row :director
+      row :archive
+      row :order
+      row :created_at
+      row :updated_at
+      row :image do
+        image_tag video.image.url(:thumb)
+      end
+    end
+  end
 end
