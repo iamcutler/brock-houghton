0f882da607e28ae694f69724e59dc13a03e25768
iamcutler <iamcutler@icloud.com>
2014-09-20 10:48:52 -0700

    Precompiling font assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( mobile.css )
-Rails.application.config.assets.precompile += %w( videojs/video-js.min.css )
-Rails.application.config.assets.precompile += %w( videojs/videojs.js )
+Rails.application.config.assets.precompile += %w( mobile.css videojs/video-js.min.css videojs/videojs.js )
+# Precompile font assets
+Rails.application.config.assets.precompile += %w( little_days.eot little_days.svg little_days.ttf little_days.woff little_days.woff2 )
+Rails.application.config.assets.precompile += %w( neord.eot neord.svg neord.ttf neord.woff neord.woff2 )
