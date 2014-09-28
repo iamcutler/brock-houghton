d0a20925e59f74e80ca4c837b96b5035e8791993
iamcutler <iamcutler@icloud.com>
2014-09-28 11:18:05 -0700

    Reenabled video formats

--- app/models/video.rb
+++ app/models/video.rb
@@ -12,16 +12,9 @@ class Video < ActiveRecord::Base
 
   has_attached_file :video,
                     :styles => {
-                      :mp4 => {
-                        :geometry => "854x510", :format => 'mp4', :streaming => true,
-                        :convert_options => {
-                          :output => {
-                            :vcodec => 'mpeg4'
-                          }
-                        }
-                      }
-                      #:ogg => {:geometry => "854x510", :format => 'ogv', :streaming => true},
-                      #:webm => {:geometry => "854x510", :format => 'webm', :streaming => true}
+                      :mp4 => {:geometry => "854x510", :format => 'mp4', :streaming => true},
+                      :ogg => {:geometry => "854x510", :format => 'ogv', :streaming => true},
+                      :webm => {:geometry => "854x510", :format => 'webm', :streaming => true}
                     },
                     :path => ":rails_root/public/system/media/uploads/:basename.:extension",
                     :processors => [:ffmpeg, :qtfaststart]
