cd2a7e0cad7fd3ce4317210ab5e9de25ba4129f4
iamcutler <iamcutler@icloud.com>
2014-09-27 19:51:25 -0700

    Fixed credentials S3 file name.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,5 +2,5 @@ Paperclip::Attachment.default_options.merge!(
   :storage => :s3,
   :s3_protocol => "https",
   :s3_host_name => "s3-us-west-1.amazonaws.com",
-  :s3_credentials => "#{Rails.root}/config/s3.yml"
+  :s3_credentials => "#{Rails.root}/config/S3.yml"
 )
